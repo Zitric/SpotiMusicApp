b82ea0a89a97f60724bb3ae9a09b7c12134e315b
Zitric <zitrcwebdeveloper@gmail.com>
2018-06-22 00:39:57 +0200

    First commit with navbar and routes of two pages, home and search

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+// Import routes
+import { ROUTES } from './app.routes';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot( ROUTES, { useHash: true } )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
